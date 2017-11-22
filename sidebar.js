2b89193dc6b44b0f6d986d2d36eb95e5e5a03acc
xernyuii <hfutxc.xern@gmail.com>
2017-11-23 00:52:02 +0800

    init@2017/11/23 for myweb.fcu.edu.tw

---
+++ sidebar.js
@@ -0,0 +1,21 @@
+document.writeln('\
+<div class="col-xs-6 col-sm-3 sidebar-offcanvas float-right" style="position:fixed;right:60px" id="sidebar">\
+        <hr>\
+        <div>\
+          <h3>作業列表</h3>\
+        </div>\
+        <div class="list-group">\
+          <a href="prolog1.html" class="list-group-item ">Prolog 練習內容</a>\
+          <a href="prolog2.html" class="list-group-item">Prolog Deficiencies</a>\
+          <a href="lisp1.html" class="list-group-item">Lisp 操作練習</a>\
+          <a href="lisp2.html" class="list-group-item">Lisp 延伸語系討論</a>\
+          <a href="test.html" class="list-group-item">期中考題目</a>\
+          <a href="#" class="list-group-item">Link</a>\
+          <a href="#" class="list-group-item">Link</a>\
+          <a href="#" class="list-group-item">Link</a>\
+          <a href="#" class="list-group-item">Link</a>\
+          <a href="#" class="list-group-item">Link</a>\
+        </div>\
+        <div><p>謝安冬 D0661526 </p></div>\
+</div><!--/.sidebar-offcanvas\
+')
