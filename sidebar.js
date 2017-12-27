736f4b5340d43b95260c373051da5ea66767629a
xernyuii <hfutxc.xern@gmail.com>
2017-12-27 15:46:44 +0800

    add new paper

--- sidebar.js
+++ sidebar.js
@@ -10,8 +10,8 @@ document.writeln('\
           <a href="lisp1.html" class="list-group-item">Lisp 操作練習</a>\
           <a href="lisp2.html" class="list-group-item">Lisp 延伸語系討論</a>\
           <a href="test.html" class="list-group-item">期中考題目</a>\
-          <a href="#" class="list-group-item">Link</a>\
-          <a href="#" class="list-group-item">Link</a>\
+          <a href="exception.html" class="list-group-item">Exception and Event Handling</a>\
+          <a href="concurrency.html" class="list-group-item">Concurrency</a>\
           <a href="#" class="list-group-item">Link</a>\
           <a href="#" class="list-group-item">Link</a>\
           <a href="#" class="list-group-item">Link</a>\
